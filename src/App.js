3d8d049a438f3916f9c9f6741df808a7b93253eb
Santiago Velosa <santiagovelosa@gmail.com>
2022-05-13 16:45:40 -0500

    Implement communication between form and server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchToken } from './redux/Auth/auth';
-import { fetchHotels, putHotels } from './redux/Hotel/hotel';
+import { fetchHotels } from './redux/Hotel/hotel';
 
 import './App.css';
 import Header from './components/Header/Header';
@@ -14,20 +14,21 @@ const App = () => {
   useEffect(() => {
     dispatch(fetchToken());
     dispatch(fetchHotels());
-    dispatch(putHotels({
-      name: 'Four Seasons',
-      location: 'Spain',
-      description: 'This is the best Four Seasons hotel in the world',
-      rating: 5,
-      image: 'https://pix10.agoda.net/hotelImages/5547513/-1/555ac3a73a392de501fd0a0b50d8fdff.jpg?ca=7&ce=1&s=1024x768',
-      rooms: [
-        {
-          id: 1,
-          price: 500,
-          image: 'Image',
-        },
-      ],
-    }));
+    // dispatch(putHotels(
+    //   {
+    //   name: 'Four Seasons',
+    //   location: 'Spain',
+    //   description: 'This is the best Four Seasons hotel in the world',
+    //   rating: 5,
+    //   image: 'https://pix10.agoda.net/hotelImages/5547513/-1/555ac3a73a392de501fd0a0b50d8fdff.jpg?ca=7&ce=1&s=1024x768',
+    //   rooms: [
+    //     {
+    //       id: 1,
+    //       price: 500,
+    //       image: 'Image',
+    //     },
+    //   ],
+    // }));
     dispatch(fetchRooms());
   }, [dispatch, fetchToken]);
 
--- src/components/AddHotel/AddHotel.jsx
+++ src/components/AddHotel/AddHotel.jsx
@@ -1,17 +1,102 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useReducer } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { putHotels } from '../../redux/Hotel/hotel';
+
+const form = {
+  name: '',
+  location: '',
+  description: '',
+  rating: 0,
+  image: '',
+  rooms: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'name':
+      return {
+        ...state,
+        name: action.payload,
+      };
+    case 'location':
+      return {
+        ...state,
+        location: action.payload,
+      };
+    case 'description':
+      return {
+        ...state,
+        description: action.payload,
+      };
+    case 'rating':
+      return {
+        ...state,
+        rating: action.payload,
+      };
+    case 'image':
+      return {
+        ...state,
+        image: action.payload,
+      };
+    default:
+      throw new Error();
+  }
+};
 
 const AddHotel = () => {
+  const dispatcher = useDispatch();
   const rooms = useSelector((state) => state.rooms.rooms);
   const roomsCheckedState = {};
   rooms.forEach((room) => {
-    roomsCheckedState[room.id] = false;
+    roomsCheckedState[room.id] = {
+      checked: false,
+      price: 0,
+      image: '',
+    };
   });
   const [checkedState, setCheckedState] = useState(roomsCheckedState);
+  const [formState, dispatch] = useReducer(reducer, form);
+  const navigate = useNavigate();
+
   const changeCheckboxHandler = (index) => {
     setCheckedState((prevState) => {
       const newState = { ...prevState };
-      newState[index] = !prevState[index];
+      newState[index] = {
+        ...prevState[index],
+        checked: !prevState[index]?.checked,
+      };
+      return newState;
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    Object.keys(checkedState).forEach((id) => {
+      if (checkedState[id].checked) {
+        formState.rooms.push({
+          id,
+          price: checkedState[id].price,
+          image: checkedState[id].image,
+        });
+      }
+    });
+    dispatcher(putHotels(formState));
+    navigate('/hotels');
+  };
+
+  const priceChangeHandler = (price, id) => {
+    setCheckedState((prevState) => {
+      const newState = { ...prevState };
+      newState[id].price = parseInt(price, 10);
+      return newState;
+    });
+  };
+
+  const roomImageChangeHandler = (image, id) => {
+    setCheckedState((prevState) => {
+      const newState = { ...prevState };
+      newState[id].image = image;
       return newState;
     });
   };
@@ -19,31 +104,32 @@ const AddHotel = () => {
   return (
     <div className="container">
       <h1 className="form-title">Add Hotel</h1>
-      <form className="hotel-form">
-        <input className="hotel-input" type="text" placeholder="Enter hotel name" />
-        <input className="hotel-input" type="text" placeholder="Enter hotel location" />
-        <textarea className="hotel-input" id="hotel-description" placeholder="Enter hotel description" />
-        <input className="hotel-input" type="number" placeholder="Enter hotel rating" />
-        <label id="file-button" className="hotel-input" htmlFor="files">
+      <form onSubmit={submitHandler} className="hotel-form">
+        <input required onChange={(e) => dispatch({ type: 'name', payload: e.target.value })} name="name" className="hotel-input" type="text" placeholder="Enter hotel name" />
+        <input required onChange={(e) => dispatch({ type: 'location', payload: e.target.value })} className="hotel-input" type="text" placeholder="Enter hotel location" />
+        <textarea required onChange={(e) => dispatch({ type: 'description', payload: e.target.value })} className="hotel-input" id="hotel-description" placeholder="Enter hotel description" />
+        <input required onChange={(e) => dispatch({ type: 'rating', payload: e.target.value })} className="hotel-input" type="number" placeholder="Enter hotel rating" />
+        <label className="hotel-input file-button" htmlFor="hotel-files">
           Upload file
-          <input id="files" className="hotel-input" type="file" />
+          <input id="hotel-files" onChange={(e) => dispatch({ type: 'image', payload: e.target.value })} className="hotel-input files" type="file" />
         </label>
         {rooms.map((room) => (
           <div key={room.id} className="checkbox">
-            <input value={room.id} name={room.type} type="checkbox" checked={checkedState[room.id] || false} onChange={() => changeCheckboxHandler(room.id)} />
+            <input value={room.id} name={room.type} type="checkbox" checked={checkedState[room.id]?.checked || false} onChange={() => changeCheckboxHandler(room.id)} />
             <label htmlFor={room.type}>{room.type}</label>
-            {checkedState[room.id]
+            {checkedState[room.id]?.checked
               && (
                 <div className="checkbox-input">
-                  <input className="hotel-input room-price" type="text" placeholder="Enter room price" />
-                  <label id="file-button" className="hotel-input room-price" htmlFor="files">
+                  <input onChange={(e) => priceChangeHandler(e.target.value, room.id)} className="hotel-input room-price" type="number" placeholder="Enter room price" />
+                  <label className="hotel-input room-price file-button" htmlFor={room.id}>
                     Upload file
-                    <input id="files" className="hotel-input" type="file" />
+                    <input id={room.id} onChange={(e) => roomImageChangeHandler(e.target.value, room.id)} className="hotel-input files" type="file" />
                   </label>
                 </div>
               )}
           </div>
         ))}
+        <button className="hotel-input file-button" type="submit">Submit</button>
       </form>
     </div>
   );
