8d5b4475edb7b246c7ac5917ece2c872e4ccde8a
Santiago Velosa <santiagovelosa@gmail.com>
2022-05-13 12:40:53 -0500

    Initial page styling

--- src/components/AddHotel/AddHotel.jsx
+++ src/components/AddHotel/AddHotel.jsx
@@ -17,23 +17,29 @@ const AddHotel = () => {
   };
 
   return (
-    <div>
-      <h1>Add Hotel</h1>
-      <form>
-        <input type="text" placeholder="Enter hotel name" />
-        <input type="text" placeholder="Enter hotel location" />
-        <textarea placeholder="Enter hotel description" />
-        <input type="number" placeholder="Enter hotel rating" />
-        <input type="file" />
+    <div className="container">
+      <h1 className="form-title">Add Hotel</h1>
+      <form className="hotel-form">
+        <input className="hotel-input" type="text" placeholder="Enter hotel name" />
+        <input className="hotel-input" type="text" placeholder="Enter hotel location" />
+        <textarea className="hotel-input" id="hotel-description" placeholder="Enter hotel description" />
+        <input className="hotel-input" type="number" placeholder="Enter hotel rating" />
+        <label id="file-button" className="hotel-input" htmlFor="files">
+          Upload file
+          <input id="files" className="hotel-input" type="file" />
+        </label>
         {rooms.map((room) => (
-          <div key={room.id}>
+          <div key={room.id} className="checkbox">
             <input value={room.id} name={room.type} type="checkbox" checked={checkedState[room.id] || false} onChange={() => changeCheckboxHandler(room.id)} />
             <label htmlFor={room.type}>{room.type}</label>
             {checkedState[room.id]
               && (
-                <div>
-                  <input type="text" placeholder="Enter room price" />
-                  <input type="file" />
+                <div className="checkbox-input">
+                  <input className="hotel-input room-price" type="text" placeholder="Enter room price" />
+                  <label id="file-button" className="hotel-input room-price" htmlFor="files">
+                    Upload file
+                    <input id="files" className="hotel-input" type="file" />
+                  </label>
                 </div>
               )}
           </div>
