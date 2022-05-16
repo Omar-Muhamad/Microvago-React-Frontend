d2bb21d95d5084a18f08abb5d439eb8e35c2aca1
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-05-16 21:54:57 +0200

    Style Header and main page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { checkAuth } from './redux/Auth/auth';
 import './App.css';
 import Header from './components/Header/Header';
-import Hotel from './components/Hotel/Hotel';
+import Hotel from './components/Hotels/Hotels';
 import Splash from './components/Splash/Splash';
 
 import Login from './components/Login/Login';
@@ -21,29 +21,31 @@ const App = () => {
   }, []);
 
   return (
-    <>
+    <div className="grid grid-cols-5">
       {isAuthorized && <Header />}
-      <Routes history={history}>
-        {isAuthorized && (
-          <>
-            <Route path="/" element={<Hotel />} />
-            {/* <Route path="/splash" element={<Splash />} /> */}
-            {/* <Route path="/addReservation" element={<AddReservation />} />
-        <Route path="/reservaions" element={<MyReservations />} />
-        <Route path="/addHotel" element={<AddHotel />} />
-        <Route path="/delHotel" element={<DelHotel />} /> */}
-          </>
-        )}
-        {!isAuthorized && (
-          <>
-            <Route path="/" element={<Splash />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="*" element={<Splash />} />
-          </>
-        )}
-      </Routes>
-    </>
+      <div className="col-span-4">
+        <Routes history={history}>
+          {isAuthorized && (
+            <>
+              <Route path="/" element={<Hotel />} />
+              {/* <Route path="/splash" element={<Splash />} /> */}
+              {/* <Route path="/addReservation" element={<AddReservation />} />
+          <Route path="/reservaions" element={<MyReservations />} />
+          <Route path="/addHotel" element={<AddHotel />} />
+          <Route path="/delHotel" element={<DelHotel />} /> */}
+            </>
+          )}
+          {!isAuthorized && (
+            <>
+              <Route path="/" element={<Splash />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="*" element={<Splash />} />
+            </>
+          )}
+        </Routes>
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/Hotels/Hotels.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { fetchHotels } from '../../redux/Hotel/hotel';
+
+const Hotel = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchHotels());
+  }, []);
+
+  const hotels = useSelector((state) => state.hotels.hotels);
+  const [count, setCount] = useState(0);
+
+  const currentHotels = hotels.slice(3 * count, 3 * count + 3);
+
+  const handleNext = () => {
+    setCount(count + 1);
+  };
+
+  const handlePrev = () => {
+    if (count <= 0) {
+      setCount(0);
+    } else {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen max-h-screen overflow-y-auto">
+      <h1 className="text-4xl font-bold m-16">Hotel list here</h1>
+      <button type="button" onClick={handlePrev}>
+        Prev
+      </button>
+      <ul className="hotels flex flex-wrap gap-8">
+        {currentHotels
+          && currentHotels.map((hotel) => (
+            <li
+              key={hotel.id}
+              className="hotel max-w-[300px] min-w-[80px] hover:scale-110 box-content p-2 hover:bg-[#9867f5] hover:text-white rounded-xl"
+            >
+              <NavLink to={`hotels/${hotel.id}`}>
+                <div className="imageContainer w-[300px] h-[170px]">
+                  <img
+                    className="w-full h-full rounded-lg"
+                    src={hotel.image}
+                    alt={hotel.name}
+                  />
+                </div>
+                <div className="text-gray-400 hover:text-white">
+                  <h2 className="hotelName my-3 text-black text-lg font-black text-center">{hotel.name.toUpperCase()}</h2>
+                  <div className="text-center mb-3 text-gray-400">-----------------------</div>
+                  <p>{`Location: ${hotel.location}`}</p>
+                  <p>{`Rating: ${hotel.rating}`}</p>
+                  <p>{`Description: ${hotel.description}`}</p>
+                </div>
+              </NavLink>
+            </li>
+          ))}
+      </ul>
+      <button type="button" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Hotel;
