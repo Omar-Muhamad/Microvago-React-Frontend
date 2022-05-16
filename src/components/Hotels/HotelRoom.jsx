5e6236acc1b688cbd607a015063677ec85fec4ca
Santiago Velosa <santiagovelosa@gmail.com>
2022-05-16 18:22:06 -0500

    Fix linters

--- src/components/Hotels/HotelRoom.jsx
+++ src/components/Hotels/HotelRoom.jsx
@@ -1,25 +1,38 @@
-// import { useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchHotelsDetails } from '../../redux/Hotel/hotel';
+import PropTypes from 'prop-types';
 import { API_URL } from '../../redux/api/apiHelper';
 
-const HotelRoom = ( {hotelRoom} ) => (
-    <div>
-        <div className="hotelRoom">
-            <div className="imageContainer w-[300px] h-[170px]">
-                <img className="w-full h-full rounded-lg" src={`${API_URL}${hotelRoom?.featured_room_image?.url}`} alt={hotelRoom.name} />
-            </div>
-            <div className="text-gray-400 hover:text-white">
-                <h2 className="hotelName my-3 text-black text-lg font-black text-center">
-                    {hotelRoom.price}
-                </h2>
-                <div className="text-center mb-3 text-gray-400">
-                    -----------------------
+const HotelRoom = ({ hotelRoom }) => (
+		<div>
+		<div className="hotelRoom">
+			<div className="imageContainer w-[300px] h-[170px]">
+				<img className="w-full h-full rounded-lg" src={`${API_URL}${hotelRoom?.featured_room_image?.url}`} alt={hotelRoom.name} />
+			</div>
+			<div className="text-gray-400 hover:text-white">
+				<h2 className="hotelName my-3 text-black text-lg font-black text-center">
+					{hotelRoom.price}
+				</h2>
+				<div className="text-center mb-3 text-gray-400">
+					-----------------------
                 </div>
-            </div>
-        </div>
-    </div>
+			</div>
+		</div>
+	</div>
 );
-    
+
+HotelRoom.propTypes = {
+	hotelRoom: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+		price: PropTypes.number.isRequired,
+		featured_room_image: PropTypes.shape({
+			url: PropTypes.string.isRequired,
+		}),
+		room: PropTypes.shape({
+			number: PropTypes.number.isRequired,
+			type: PropTypes.string.isRequired,
+			guests_number: PropTypes.number.isRequired,
+		})
+  }).isRequired,
+}
+
 export default HotelRoom;
