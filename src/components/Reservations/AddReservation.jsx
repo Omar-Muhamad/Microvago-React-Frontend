c38ae3ee8d71d521706da282f13f1c8d093683bf
TheLuisAbarca <arclaav.adm@gmail.com>
2022-05-17 16:20:25 -0500

    Add submit button functionality and redirection.

--- src/components/Reservations/AddReservation.jsx
+++ src/components/Reservations/AddReservation.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation, useNavigate } from "react-router-dom";
+import { putReservation } from '../../redux/Reservation/reservation';
 
 const useQuery = () => {
   const { search } = useLocation();
@@ -13,13 +14,34 @@ const getTodaysDate = () => (
 );
 
 const AddReservation = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const hotels = useSelector((state) => state.hotels.hotels);
   const [hotelsSelectValue, setHotelsSelectValue] = useState('');
   const [hotelRoomsSelectValue, setHotelRoomsSelectValue] = useState('');
+  const [startDate, setStartDate] = useState(getTodaysDate());
 
   let query = useQuery();
   const hotelId = query.get("hotelId");
   const hotelRoomId = query.get("hotelRoomId");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(hotelRoomsSelectValue);
+    console.log(startDate.target.value);
+    const reservation = {
+      "hotel_room_id": hotelRoomsSelectValue,
+      "date": startDate.target.value
+    } 
+
+    dispatch(putReservation(reservation));
+    navigate('/myreservations');
+    // after submit navigate to my reservations
+    // const hotel = hotelsSelectValue;
+    // const hotelRoom = hotel.hotel_rooms.find((hotelRoom) => hotelRoom.id === hotelRoomsSelectValue); 
+  }
+
+
   useEffect(() => {
     if(hotelId) {
       setHotelsSelectValue(hotelId);
@@ -41,7 +63,7 @@ const AddReservation = () => {
   },[hotelsSelectValue])
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div>
       <select value={hotelsSelectValue || 'none'} onChange={(e) => setHotelsSelectValue(e.target.value)} disabled={hotelId}>
         <option disabled value='none'>Select hotel</option>
@@ -54,9 +76,10 @@ const AddReservation = () => {
           <option key={hotelRoom.id} value={hotelRoom.id}>{hotelRoom.room.type}</option>
         ))}
       </select>
-      <input type="date" min={getTodaysDate()} />
+      <input required name="reservDate" type="date" min={getTodaysDate()} onChange={setStartDate}/>
       <div>{hotels.find((hotel) => hotel.id === parseInt(hotelsSelectValue, 10))?.hotel_rooms?.find((room) => room.id == parseInt(hotelRoomsSelectValue, 10))?.price}</div>
       </div>
+      <input name="reservSubmit" type="submit" value="Book Hotel Room" /> 
     </form>
   );
 };
